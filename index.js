063bb882240cd08bc6c5816bf5f4061e868a1c20
Oluwagbemi Oluwasakin <51876261+Gbemi06@users.noreply.github.com>
2022-01-27 14:14:26 +0200

    Exercise 2 completed

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-window.onload = () => {
-  fetch("https://api.pexels.com/v1/search?query=your-query", {
+window.onload = (band) => {
+  fetch(`https://api.pexels.com/v1/search?query=${band}`, {
     headers: {
       Authorization: "563492ad6f91700001000001d73ed4a7e8f34205987bdd1ecd5d4040",
     },
   })
     .then((response) => response.json())
     .then((loadData) => {
-      //console.log(loadData);
-
-      let imageData = document.querySelector(".image_content");
+      console.log(loadData);
+    });
+};
+/*let imageData = document.querySelector(".image_content");
 
       for (let i = 0; i < loadData.photos.length; i++) {
         const image = loadData.photos[i];
@@ -17,7 +18,7 @@ window.onload = () => {
         const theData = document.createElement("div");
         theData.classList.add("col-md-4");
 
-        theData.innerHTML += `
+        theData.innerHTML = `
               <div class="card mb-4 shadow-sm">
             
                   
@@ -61,3 +62,11 @@ window.onload = () => {
       }
     });
 };
+
+const changeImage = () => {
+  loadPage("your-query");
+};
+
+const changeImage1 = () => {
+  loadPage("your-secondary-query");
+};*/
